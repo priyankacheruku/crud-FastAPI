04eb89b4d07453f497e08e34fff6cc0df261ac5b
Priyanka <priyankacheruku2016@gmail.com>
2021-08-30 19:20:05 +0530

    middlewares

--- book_store/main.py
+++ book_store/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
+from fastapi.middleware.cors import CORSMiddleware
 # from books 'package' imported  books 'file'/'APIRouter'
 from book_store.books.books import router as books_router
 from book_store.person.person_weight import router as person_router
@@ -14,5 +15,29 @@ app.include_router(books_router)
 app.include_router(person_router)
 app.include_router(user_router)
 app.include_router(login_router)
+
+import time
+
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+
+    start_time = time.time()
+
+    response = await call_next(request)
+
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+
+    return response
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
